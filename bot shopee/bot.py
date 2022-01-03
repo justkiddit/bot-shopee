6d331accd46fa58f0d49f934abde64e3d38ead54
ian <95844714+justkiddit@users.noreply.github.com>
2022-01-04 06:02:24 +0700

    Add files via upload

--- bot shopee/bot.py
+++ bot shopee/bot.py
@@ -12,10 +12,10 @@ sekarang=datetime.datetime.now()
 
 def InputData() :
     global link_produk,time_target,sekarang
-    link_produk=input('(#) Link Produk Shopee : ')
-    jam=int(input('(#) Jam Beli : '))
-    menit=int(input('(#) Menit Beli : '))
-    detik=int(input('(#) Detik Beli : '))
+    link_produk=(input(' (#) Link Produk Shopee : '))
+    jam=int(input(' (#) Jam Beli : '))
+    menit=int(input(' (#) Menit Beli : '))
+    detik=int(input(' (#) Detik Beli : '))
     time_target=datetime.datetime(sekarang.year,sekarang.month,sekarang.day,jam,menit,detik)
 
 def SetupSelenium():
@@ -27,7 +27,7 @@ def SetupSelenium():
     firefox_profile = webdriver.FirefoxProfile('system/profile')
 
     ### Buka Browser
-    browser = webdriver.Firefox(executable_path=selenium,firefox_binary=binary,firefox_profile=firefox_profile)
+    browser=webdriver.Firefox(executable_path=selenium,firefox_binary=binary,firefox_profile=firefox_profile)
 
     ### Clear Cache & Cookie
     browser.delete_all_cookies()
@@ -43,37 +43,44 @@ def click(browser,element_css):
     )
     browser.find_element_by_css_selector(element_css).click()
 
-Banner()
 InputData()
 browser=SetupSelenium()
-# buka halaman login
+# Buka halaman login
 browser.get("https://shopee.co.id/buyer/login")
 wait = WebDriverWait(browser, 60)
-# cek udah login
+# Cek udah login
 wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, '.shopee-avatar')))
-# buka produk
+# Buka produk
 browser.get(link_produk)
 
-# delay Waktu
-# selisih=time_target-datetime.datetime.now()
-# time.sleep(selisih.seconds)
-# browser.refresh()
+# Countdown timer
+countdown=time_target-datetime.datetime.now()
+time.sleep(countdown.seconds)
+browser.refresh()
 
-# Button Beli
+# Button Tipe
+click(browser,'button.product-variation:nth-child(2)')
+
+# Button Beli Sekarang
 click(browser,'button.btn--l:nth-child(2)')
 WebDriverWait(browser, 60).until(
     ec.invisibility_of_element((By.CSS_SELECTOR, '.action-toast'))
 )
-# Button checkout
+# Button Checkout
 click(browser,'.shopee-button-solid')
 WebDriverWait(browser, 60).until(
     ec.invisibility_of_element((By.CSS_SELECTOR, '.loading-spinner-popup'))
 )
-# Transfer Bank BRI
+# Metode Pembayaran via Shopee Pay
 click(browser,'.checkout-payment-setting__payment-methods-tab > span:nth-child(2) > button:nth-child(1)')
-click(browser,'div.bank-transfer-category:nth-child(1) > div:nth-child(2) > div:nth-child(4) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)')
+click(browser,'.checkout-bank-transfer-item__card')
 WebDriverWait(browser, 60).until(
     ec.invisibility_of_element((By.CSS_SELECTOR, '.loading-spinner-popup'))
 )
 # Buat Pesanan
-click(browser,'.stardust-button')
+click(browser,'button.stardust-button:nth-child(2)')
+WebDriverWait(browser, 60).until(
+    ec.invisibility_of_element((By.CSS_SELECTOR, '.loading-spinner-popup__container'))
+)
+# Konfirmasi pembayaran via Shopee Pay
+click(browser,'#pay-button')
